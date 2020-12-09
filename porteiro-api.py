32dd73c175329a6d32d458c6057b9b7f33a99fba
Thiago Gonçalo <thiago701@gmail.com>
2020-12-08 23:59:42 -0300

    Add files via upload

--- porteiro-api.py
+++ porteiro-api.py
@@ -1,6 +1,8 @@
 from flask import Flask,jsonify,request
+import pandas as pd
+import csv, os
+import http
 
-mensagem = ''
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,14 +19,42 @@ def receberFala(audio):
 @app.route('/api/notificar')
 def notificarMorador():
     print('teste notificar')
-    mensagem = request.args['apartamento']
+    msg = 'Visita para apt {}!'.format(request.args['apartamento'])
+    salvarMensagem(msg)
     return 'Testa notificação'
 
 #cloud
 @app.route('/api/notificacoes')
 def obterNotificacoes():
-    print('msg:',mensagem)
-    if(mensagem != None):
-        return (mensagem)
+    
+    if(not os.path.exists(os.path.join(os.getcwd(),'dados.csv'))):
+        df = pd.DataFrame(columns=['mensagem','status'])
+        df.to_csv('dados.csv',index=False)
+    
+    base = pd.read_csv('dados.csv')
+    mensagens = base.query('status == 0')
+    dicio = mensagens.to_dict()
+    
+    # atualizar status
+    atualizarStatus(base)
+    
+    if(mensagens.empty):
+        return '', http.HTTPStatus.NO_CONTENT
+    elif(dicio):
+        print('retornando mensagens...',dicio)
+        return jsonify(dicio['mensagem'][1])
+
+# fghsfjhghlkjsf
+def salvarMensagem(msg):
+    if(not os.path.exists(os.path.join(os.getcwd(),'dados.csv'))):
+        df = pd.DataFrame(columns=['mensagem','status'])
+        df.to_csv('dados.csv',index=False)
+    base = pd.read_csv('dados.csv')    
+    novaMensagem = pd.DataFrame([[msg,0]], columns=['mensagem','status'])
+    baseAtualizada = base.append(novaMensagem, ignore_index=True)
+    baseAtualizada.to_csv('dados.csv', index=False)
+    
         
-        
+def atualizarStatus(dataFrame):
+    dataFrame['status'] = 1
+    dataFrame.to_csv('dados.csv', index=False)
--- porteiro-interface.py
+++ porteiro-interface.py
@@ -1,7 +1,12 @@
 import requests
+import pandas as pd
 
 numeroApt = input('Apartamento: ')
 print('Informado:',numeroApt)
 
 payload = {'id': '123ABC', 'apartamento': numeroApt}
 r = requests.get('http://127.0.0.1:5000/api/notificar',params=payload)
+
+
+
+
